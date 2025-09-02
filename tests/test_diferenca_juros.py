c1219e7546895b1f778af3aabbd0f9f091bf167b
EmilyRochaTascaSenai <emily.rocha192@gmail.com>
2025-09-02 10:16:08 -0300

    arrumando os test

--- tests/test_diferenca_juros.py
+++ tests/test_diferenca_juros.py
@@ -1,16 +1,15 @@
 import pytest
 from calculadora_financeira import calcular_diferenca_juros
 
-
-
+# Testes com entradas válidas
 def test_valores_validos():
-    assert calcular_diferenca_juros(1000, 5, 2) == 2.5
+    assert calcular_diferenca_juros(1000, 5, 2) == pytest.approx(2.5, 0.01)
 
-def test_todos_zeros ():
-    assert calcular_diferenca_juros (0,0,0) ==0.0
+def test_todos_zeros():
+    assert calcular_diferenca_juros(0, 0, 0) == 0.0
 
-def  test_juros_e_tempo_zero ():
-    assert calcular_diferenca_juros(1000,0,0) == 0.0
+def test_juros_e_tempo_zero():
+    assert calcular_diferenca_juros(1000, 0, 0) == 0.0
 
 def test_tempo_zero():
     assert calcular_diferenca_juros(1000, 5, 0) == 0.0
@@ -19,22 +18,22 @@ def test_taxa_zero():
     assert calcular_diferenca_juros(1000, 0, 5) == 0.0
 
 def test_numero_decimal():
-    assert calcular_diferenca_juros(1500.50, 4.5, 3.2) == 34.57
+    assert calcular_diferenca_juros(1500.50, 4.5, 3.2) == pytest.approx(34.57, 0.01)
 
-# Testes com entradas inválidas
+
+# Testes com entradas inválidas (strings, negativos)
 def test_entrada_string():
-    with pytest.raises(ValueError,match="Valor Inválido"):
-           calcular_diferenca_juros("ola", 5, 2)
-
+    with pytest.raises(ValueError, match="números"):
+        calcular_diferenca_juros("ola", 5, 2)
 
 def test_capital_negativo():
-    with pytest.raises(ValueError,match="Valor Inválido"):
-        calcular_diferenca_juros(-1000,5,2)
+    with pytest.raises(ValueError, match="não podem ser negativos"):
+        calcular_diferenca_juros(-1000, 5, 2)
 
-def test_taxa_negativa ():
-        with pytest.raises(ValueError,match="Valor Inválido"):
-             calcular_diferenca_juros(1000,-5,2)
+def test_taxa_negativa():
+    with pytest.raises(ValueError, match="não podem ser negativos"):
+        calcular_diferenca_juros(1000, -5, 2)
 
-def test_tempo_negativo ():
-     with pytest.raises(ValueError,match="Valor Inválido"):
-          calcular_diferenca_juros(1000,5,-2)
+def test_tempo_negativo():
+    with pytest.raises(ValueError, match="não podem ser negativos"):
+        calcular_diferenca_juros(1000, 5, -2)
