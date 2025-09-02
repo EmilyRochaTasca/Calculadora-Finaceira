6d4a4723fa895aef9d37a24437ee119bb7b27092
EmilyRochaTascaSenai <emily.rocha192@gmail.com>
2025-09-02 09:39:08 -0300

    Fazendo os tests

--- tests/test_diferenca_juros.py
+++ tests/test_diferenca_juros.py
@@ -3,3 +3,39 @@ from calculadora_financeira import calcular_diferenca_juros
 
 
 
+# Teste com valores válidos
+def test_valores_validos():
+    assert calcular_diferenca_juros(1000, 5, 2) == 2.5
+
+def test_todos_zeros ():
+    assert calcular_diferenca_juros (0,0,0) ==0.0
+
+def  test_juros_e_tempo_zero ():
+    assert calcular_diferenca_juros(1000,0,0) == 0.0
+
+def test_tempo_zero():
+    assert calcular_diferenca_juros(1000, 5, 0) == 0.0
+
+def test_taxa_zero():
+    assert calcular_diferenca_juros(1000, 0, 5) == 0.0
+
+def test_numero_decimal():
+    assert calcular_diferenca_juros(1500.50, 4.5, 3.2) == 34.57
+
+# Testes com entradas inválidas
+def test_entrada_string():
+    with pytest.raises(ValueError,match="Valor Inválido"):
+           calcular_diferenca_juros("ola", 5, 2)
+
+
+def test_capital_negativo():
+    with pytest.raises(ValueError,match="Valor Inválido"):
+        calcular_diferenca_juros(-1000,5,2)
+
+def test_taxa_negativa ():
+        with pytest.raises(ValueError,match="Valor Inválido"):
+             calcular_diferenca_juros(1000,-5,2)
+
+def test_tempo_negativo ():
+     with pytest.raises(ValueError,match="Valor Inválido"):
+          calcular_diferenca_juros(1000,5,-2)
